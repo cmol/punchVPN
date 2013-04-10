e82f3d22576617e8161f9a00f9d8ee6fd0c44ebc
Claus Lensbøl <cmol@cmol.dk>
2013-04-10 15:52:27 +0200

    Edit: Is now a dynamic class

--- punchVPN/udpKnock.py
+++ punchVPN/udpKnock.py
@@ -1,18 +1,27 @@
 import socket
 import errno
 
-class udpKnock:
-    @staticmethod
-    def knock(s, addr, lport, rport):
-        """Connect to $addr with lport as local port.
-        Try until a port is useable"""
+class udpKnock(object):
+    
+    def __init__(self, s, lport):
+        """docstring for __init__"""
         while(1):
             try:
                 s.bind(('', lport))
+                break
             except socket.error as e:
                  if e.errno != errno.EADDRINUSE:
                      raise e
                  lport+=1
-            else:
-                s.connect((addr, rport))
-                return s, lport
+        self.lport = lport
+        self.s = s
+
+    def lport(self):
+        return self.lport
+
+    def knock(self, addr, rport):
+        """Connect to $addr with lport as local port.
+        Try until a port is useable"""
+        self.s.connect((addr, rport))
+        self.s.sendto(bytes('knock knock', 'UTF-8'), (addr, rport))
+        return self.s
