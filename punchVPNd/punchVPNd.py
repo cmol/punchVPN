87e130531b1dbe2cc4a49b57aa06dd68b8f78f3e
Claus Lensbøl <cmol@cmol.dk>
2013-04-12 12:33:46 +0200

    Testing webserver connections 	modified:   punchVPNd/punchVPNd.py

--- punchVPNd/punchVPNd.py
+++ punchVPNd/punchVPNd.py
@@ -1,8 +1,17 @@
 from bottle import route, run
+from random import randint
 
-
-@route('/hello')
+@route('/')
 def hello():
-    return "Hello World!"
+    return hex(randint(4096, 65535))
+
+@route('/me/<token>')
+def me(token):
+    return "Waiting for partner wanting token: "+token
+
+@route('/connect/<token>')
+def connect(token):
+    return "Partner "+token+" is waiting for you"
+
 
 run(host='localhost', port=8080, debug=True)
