cb818c92630e3f1b3d4caeb384fc536ed57c1caf
Claus Lensbøl <cmol@cmol.dk>
2013-04-14 15:20:16 +0200

    Added a get method 	modified:   punchVPN/WebConnect.py

--- punchVPN/WebConnect.py
+++ punchVPN/WebConnect.py
@@ -1,4 +1,5 @@
 import urllib.request
+import urllib.parse
 
 class WebConnect(object):
     def __init__(self, host, lport):
@@ -8,7 +9,16 @@ class WebConnect(object):
 
     def get(self, path):
         """GET an URL, and check for status codes"""
-        respons = urllib.request.urlopen(self.host+path)
+        return self.request(path)
+
+    def post(self, path, post_data):
+        return self.request(path, post_data)
+
+    def request(self, path, data=None):
+        if data:
+            respons = urllib.request.urlopen(self.host+path,urllib.parse.urlencode(data).encode('utf-8'))
+        else:
+            respons = urllib.request.urlopen(self.host+path)
         content = respons.read().decode("UTF-8")
         respons.close()
         return content
