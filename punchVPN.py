1c413fac6988ec7ef9c9f2bd563a05fbc5ca4685
Claus Lensbøl <cmol@cmol.dk>
2013-04-14 15:21:12 +0200

    The prototype is working with long polling, token matching and port exchanging. 	modified:   punchVPN.py 	modified:   punchVPNd/punchVPNd.py

--- punchVPN.py
+++ punchVPN.py
@@ -4,6 +4,17 @@ import socket
 from random import randint
 from punchVPN.udpKnock import udpKnock
 from punchVPN.WebConnect import WebConnect
+import argparse
+
+parser = argparse.ArgumentParser(prog='snake.py',
+                                formatter_class=argparse.ArgumentDefaultsHelpFormatter,
+                                description='Small snake game for many players')
+parser.add_argument('-p', '--peer', type=str, default=None, help='Token of your peer')
+parser.add_argument('-c', '--client', action='store_true', help='Is this a client?')
+parser.add_argument('-a', '--address', type=str, default='http://localhost:8080', help='What is the server address?')
+args = parser.parse_args()
+
+peer = args.peer
 
 def log(m):
     print(m)
@@ -21,10 +32,23 @@ web = WebConnect(thrdPrtyHost, lport)
 
 token = web.get("/")
 log(token)
-log(web.get("/me/"+token))
+
+if peer:
+    log(web.post("/connect/",
+        {'token': peer,
+         'lport': lport,
+         'uuid':  token}))
+    """This is where we are supposed to start the openVPN client"""
+else:
+    log(web.post("/me/",
+        {'uuid': token,
+         'lport' : lport}))
+    """This is where we are supposed to port knock, and start the openVPN server"""
+    log(web.post("/ready/",
+        {'uuid': token}))
 
 # Use socket, and connect to the other end
 raddr = "8.8.8.8"
 rport = 12345
 s = knocker.knock(raddr, rport)
-log("rport, lport is: "+str(rport)+", "+str(lport))
+#log("rport, lport is: "+str(rport)+", "+str(lport))
--- punchVPNd/punchVPNd.py
+++ punchVPNd/punchVPNd.py
@@ -1,17 +1,116 @@
-from bottle import route, run
-from random import randint
+import inspect
+import uuid
+from gevent import monkey; monkey.patch_all()
+from gevent.event import Event
+from beaker.middleware import SessionMiddleware
+import bottle
+from bottle import route, request, static_file, template, run
+from time import sleep
+
+def log(m):
+    if False:
+        print m
+
+class BeakerPlugin(object):
+    name = 'beaker'
+
+    def setup(self, app):
+        ''' Make sure that other installed plugins don't affect the same
+            keyword argument.'''
+        for other in app.plugins:
+            if not isinstance(other, BeakerPlugin): continue
+            if other.keyword == self.keyword:
+                raise PluginError("Found another beaker session plugin "\
+                "with conflicting settings (non-unique keyword).")
+
+    def apply(self, callback, context):
+        args = inspect.getargspec(context['callback'])[0]
+        keyword = 'session'
+        if keyword not in args:
+            return callback
+        def wrapper(*a, **ka):
+            session = request.environ.get('beaker.session')
+            ka[keyword] = session
+            rv = callback(*a, **ka)
+            session.save()
+            return rv
+        return wrapper
+
+class Peer(object):
+    def __init__(self, ip, lport):
+        """Set up peer object"""
+        self.ip = ip
+        self.lport = lport
+        self.peer = None
+
+peers = {}
+new_connect_event = Event()
+new_request_event = Event()
 
 @route('/')
 def hello():
-    return hex(randint(4096, 65535))
+    return str(uuid.uuid4()).split("-")[1]
 
-@route('/me/<token>')
-def me(token):
-    return "Waiting for partner wanting token: "+token
+@route('/me/', method='POST')
+def me():
+    global new_request_event
+    global peers
+    me = Peer(request.environ.get('REMOTE_ADDR'), request.POST.get('lport'))
+    peers[request.POST.get('uuid')] = me
+    while(1):
+        log("Peer '"+request.POST.get('uuid')+"' is waiting")
+        new_request_event.wait()
+        if me.peer:
+            return ",".join((me.peer.ip, me.peer.lport))
 
-@route('/connect/<token>')
-def connect(token):
-    return "Partner "+token+" is waiting for you"
+@route('/connect/', method='POST')
+def connect():
+    global new_request_event
+    global new_connect_event
+    global peers
+    token = request.POST.get('token')
+    if not peers.has_key(token):
+        return "NOT_CONNECTED"
+    me = Peer(request.environ.get('REMOTE_ADDR'), request.POST.get('lport'))
+    peers[request.POST.get('uuid')] = me
+    peers[token].peer = me
+    new_request_event.set()
+    new_request_event.clear()
+    while(1):
+        log("Peer '"+request.POST.get('uuid')+"' requested '"+token+"'")
+        new_connect_event.wait()
+        if me.peer:
+            msg = ",".join((me.peer.ip, me.peer.lport))
+            del peers[request.POST.get('uuid')]
+            del peers[token]
+            return msg
+
+@route('/ready/', method='POST')
+def ready():
+    global new_connect_event
+    global peers
+    me = peers[request.POST.get('uuid')]
+    me.peer.peer = me
+    log("Peer '"+request.POST.get('uuid')+"' is ready")
+    new_connect_event.set()
+    new_connect_event.clear()
+    return "OK"
+
+app = bottle.app()
+app.install(BeakerPlugin())
+
+session_opts = {
+    'session.type': 'file',
+    'session.cookie_expires': 300,
+    'session.data_dir': './data',
+    'session.auto': True
+}
+app = SessionMiddleware(app, session_opts)
 
 
-run(host='localhost', port=8080, debug=True)
+if __name__ == '__main__':
+    bottle.debug(True)
+    bottle.run(app=app, server='gevent', host='0.0.0.0')
+
+
+#run(host='localhost', port=8080, debug=True)
